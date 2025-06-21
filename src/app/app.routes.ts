50718a79b0fd5e18f910a037404a55190410faa4
LucaSoftwareEngineer <lucapc40@gmail.com>
2025-06-21 12:01:35 +0200

    implementato template login e template register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Login } from './pages/login/login.component';
+import { Register } from './pages/register/register.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: Login },
-  { path: 'dashboard', component: DashboardComponent},
+  { path: 'register', component: Register },
+  { path: 'dashboard', component: DashboardComponent },
 ];
---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class Register {
+
+  username: string = '';
+  password: string = '';
+
+  constructor () {}
+
+  register() {
+
+  }
+
+}
