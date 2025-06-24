aa9c3a81ccb5a561dd8a9312cb4fbce105212df9
LucaSoftwareEngineer <lucapc40@gmail.com>
2025-06-24 05:02:20 +0200

    implementato sistema autenticazione che si interfaccia con il backend

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,11 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { LoginService } from './../../services/login.service';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, ErrorHandler } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import SecureLS from 'secure-ls';
+import { catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -14,10 +17,45 @@ import { Router, RouterLink } from '@angular/router';
 export class Login {
   username: string = '';
   password: string = '';
+  warning: string = '';
+  ls = new SecureLS();
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private router: Router, private loginService: LoginService) {}
 
   login() {
-    this.router.navigate(['/dashboard']);
+    let nErr = 0;
+
+    if (this.username == '') {
+      this.warning += 'username mancante';
+      nErr++;
+    }
+
+    if (this.password == '') {
+      if (nErr == 0) {
+        this.warning += 'password mancante';
+      } else {
+        this.warning = 'username e password mancanti';
+      }
+      nErr++;
+    }
+
+    if (nErr == 0) {
+      this.ls.remove('user');
+      this.loginService.login(this.username, this.password).subscribe({
+        next: (authResponse) => {
+          if (authResponse.token != '' && authResponse.token != null) {
+            this.ls.set('user', authResponse.token);
+            this.router.navigate(['/dashboard']);
+          }
+        },
+        error: (authError) => {
+          this.warning = 'Attenzione! username o password errati...';
+        },
+      });
+    }
+  }
+
+  resetWarning() {
+    this.warning = '';
   }
 }
